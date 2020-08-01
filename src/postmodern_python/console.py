80695d5771921d2986580f90d1e1501160d35023
Ian M <ian@telem.us>
2020-08-01 12:12:46 -0500

    Add language option

--- src/postmodern_python/console.py
+++ src/postmodern_python/console.py
@@ -2,17 +2,24 @@
 import textwrap
 
 import click
-import requests
 
 from . import __version__, wikipedia
 
 API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 
 @click.command()
+@click.option(
+    "--language",
+    "-l",
+    default="en",
+    help="Language edition of Wikipedia",
+    metavar="LANG",
+    show_default=True,
+)
 @click.version_option(version=__version__)
-def main():
+def main(language):
     """the postmodern python jam."""
-    data = wikipedia.random_page()
+    data = wikipedia.random_page(language=language)
 
     title = data["title"]
     extract = data["extract"]
